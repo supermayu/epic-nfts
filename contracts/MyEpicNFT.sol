b8f4d19df1f943f5d8c459606d5392483a90a66a
mayu-jp-20 <yoshidamayu1221@gmail.com>
2023-01-22 04:51:50 +0900

    add create nft function

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -1,9 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyEpicNFT {
-    constructor() {
+contract MyEpicNFT is ERC721URIStorage {
+
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721 ("TanyaNFT", "TANYA") {
         console.log("This is my NFT contract.");
+    }
+
+    function makeAnEpicNFT() public {
+
+        uint256 newItemId = _tokenIds.current();
+
+        _safeMint(msg.sender, newItemId);
+
+        _setTokenURI(newItemId, "https://api.npoint.io/ae36305723652262fa9e");
+
+        console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+
+        _tokenIds.increment();
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -5,6 +5,11 @@ const main = async () => {
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
     console.log("Contract deployed to:", nftContract.address);
+
+    let txn = await nftContract.makeAnEpicNFT();
+    await txn.wait();
+    txn = await nftContract.makeAnEpicNFT();
+    await txn.wait();
 };
 
 const runMain = async () => {
